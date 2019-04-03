5707ffb47d2e995881e5277dfa49a6a4825f5c3d
Alvise Susmel <alvise@poeticoding.com>
2019-04-03 11:35:58 +0100

    HTTPStream.get/2 with emit_end option

--- lib/http_stream.ex
+++ lib/http_stream.ex
@@ -1,18 +1,56 @@
 defmodule HTTPStream do
-  @moduledoc """
-  Documentation for HTTPStream.
-  """
 
-  @doc """
-  Hello world.
+  def get(url,emit_end\\false) do
 
-  ## Examples
+    Stream.resource(
+      fn -> start_fun(url) end,
 
-      iex> HTTPStream.hello()
-      :world
+      # next_fun (multi caluses)
+      fn 
+        %HTTPoison.AsyncResponse{}=resp ->
+          handle_async_resp(resp,emit_end)
 
-  """
-  def hello do
-    :world
+        #last accumulator when emitting :end
+        {:end, resp}->
+          {:halt, resp}
+      end,
+      
+      fn %HTTPoison.AsyncResponse{id: id} ->
+        IO.puts("END_FUN")
+        :hackney.stop_async(id)
+      end
+    )
   end
+
+
+  defp start_fun(url) do
+    HTTPoison.get!(url,%{}, [stream_to: self(), async: :once])
+  end
+
+
+  defp handle_async_resp(%HTTPoison.AsyncResponse{id: id}=resp,emit_end) do
+    receive do
+      %HTTPoison.AsyncStatus{id: ^id, code: code}->
+        IO.inspect(code, label: "STATUS: ")
+        HTTPoison.stream_next(resp)
+        {[], resp}
+      %HTTPoison.AsyncHeaders{id: ^id, headers: headers}->
+        IO.inspect(headers, label: "HEADERS: ")
+        HTTPoison.stream_next(resp)
+        {[], resp}
+      %HTTPoison.AsyncChunk{id: ^id, chunk: chunk}->
+        HTTPoison.stream_next(resp)
+        # :erlang.garbage_collect()
+        {[chunk], resp}
+      %HTTPoison.AsyncEnd{id: ^id}->
+        if emit_end do
+          {[:end], {:end, resp}}  
+        else
+          {:halt, resp}
+        end
+    after
+      5_000 -> raise "receive timeout"
+    end
+  end
+
 end
--- mix.exs
+++ mix.exs
@@ -21,8 +21,8 @@ defmodule HTTPStream.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:httpoison, "~> 1.5"},
+      {:benchee, "~> 1.0", only: :dev}
     ]
   end
 end
